07ad09d9660ead552dd14fa325383e4f4ac20b57
utkarrshgit <utkarshjaiswal142@gmail.com>
2025-12-28 19:43:17 +0530

    feat: add firebase auth context

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRF_QkLPrcs5fwe3M4Xj8aL3qCRXEd9KI",
+  authDomain: "anime-tracker-2025.firebaseapp.com",
+  projectId: "anime-tracker-2025",
+  appId: "1:513972800482:web:68daa6e8f246ed4b9887bb",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
