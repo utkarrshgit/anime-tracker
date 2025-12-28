58986b4ee50894ce9334c9e23fd965f72b12cffb
utkarrshgit <utkarshjaiswal142@gmail.com>
2025-12-28 19:43:07 +0530

    feat: add firebase auth context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { WatchlistProvider } from "./context/WatchlistContext";
+import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <WatchlistProvider>
-      <App />
-    </WatchlistProvider>
-  </StrictMode>,
-)
+    <AuthProvider>
+      <WatchlistProvider>
+        <App />
+      </WatchlistProvider>
+    </AuthProvider>
+  </StrictMode>
+);
